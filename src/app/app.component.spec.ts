a9e7a9897250965430ac7e9b80f482971c600d59
punkrocker178 <ngochieu.le1708@gmail.com>
2021-08-15 21:57:41 +0700

    Remove angular default test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-word-counter'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-word-counter');
-  });
-
 });
