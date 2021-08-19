f6fe3db910d9f2a96b336ad04462b562b2374b3b
punkrocker178 <ngochieu.le1708@gmail.com>
2021-08-19 22:24:50 +0700

    Added character counter functionality

--- src/app/components/tweet/components/tweet-box/tweet-box.component.ts
+++ src/app/components/tweet/components/tweet-box/tweet-box.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { fromEvent, Subject } from 'rxjs';
 import { debounceTime, takeUntil, tap } from 'rxjs/operators';
 import { TweetService } from 'src/app/services/tweet.service';
@@ -6,22 +6,27 @@ import { TweetService } from 'src/app/services/tweet.service';
 @Component({
   selector: 'app-tweet-box',
   templateUrl: './tweet-box.component.html',
-  styleUrls: ['./tweet-box.component.sass']
+  styleUrls: ['./tweet-box.component.sass'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TweetBoxComponent implements OnInit, OnDestroy, AfterViewInit {
   readonly MAXIMUM_WORDS_ALLOWED = 50;
   @ViewChild('tweetInput') inputEl: ElementRef;
+  @ViewChild('tweetInput2') inputEl2: ElementRef;
+  @ViewChild('btn') buttonEl: ElementRef;
+
   destroy$: Subject<boolean> = new Subject();
 
   isLoading: boolean;
   wordRegex = /^[\W]*[\w]+[\W]*$/;
   wordCount: number = 0;
+  charCount: number = 0;
   textValue: string;
 
   constructor(private tweetService: TweetService) { }
 
   ngOnInit(): void {
-    
+
   }
 
   ngAfterViewInit() {
@@ -34,8 +39,33 @@ export class TweetBoxComponent implements OnInit, OnDestroy, AfterViewInit {
       })
 
     ).subscribe(_ => {
-      setTimeout(() => this.isLoading = false, 400);
+      setTimeout(() => this.isLoading = false, 300);
     });
+
+    this.inputEl2.nativeElement.addEventListener('keyup', (event: any) => {
+      setTimeout(() => {
+        if (event.code === 'ArrowUp' || event.code === 'ArrowRight' ||
+          event.code === 'ArrowDown' || event.code === 'ArrowLeft') {
+          return;
+        }
+        this.countChar(event);
+      })
+    });
+  }
+
+  countChar(event: any) {
+    const textArea = event.target;
+    const content = textArea.textContent;
+    let text = content?.replace(/\s+/g, '');
+    this.charCount = text?.length;
+
+    if (this.charCount >= this.MAXIMUM_WORDS_ALLOWED) {
+      let normal = textArea.textContent.substring(0, this.MAXIMUM_WORDS_ALLOWED - 1);
+      let highlight = textArea.textContent.substring(this.MAXIMUM_WORDS_ALLOWED - 1);
+      const innerHTML = `${normal}<span class="highlight">${highlight}</span>`;
+      this.inputEl2.nativeElement.innerHTML = innerHTML;
+      this.setEndCaret();
+    }
   }
 
   countWord(event: any) {
@@ -57,7 +87,37 @@ export class TweetBoxComponent implements OnInit, OnDestroy, AfterViewInit {
     this.tweetService.addTweet = this.textValue;
     this.textValue = '';
     this.wordCount = 0;
+
+  }
+
+  tweet2() {
+    if (!this.inputEl2.nativeElement.textContent) {
+      this.tweetService.addTweet = 'a';
+    } else {
+      this.tweetService.addTweet = this.inputEl2.nativeElement.textContent;
+    }
+
+    this.inputEl2.nativeElement.textContent = '';
+    this.charCount = 0;
+  }
+
+
+  /* Reference: https://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser/4238971#4238971 */
+  setEndCaret() {
+    let range = document.createRange();
+      
+    // Select all content
+    range.selectNodeContents(this.inputEl2.nativeElement);
     
+    // Collapse caret: Make start caret go to end caret
+    range.collapse(false);
+    let selection = window.getSelection();
+    
+    // Remove all other selections
+    selection?.removeAllRanges();
+
+    // Select the collapse caret
+    selection?.addRange(range);
   }
 
   ngOnDestroy() {
